dc738aa6dfa73b23931f0b845d88784400233990
Salvamend <mendes@mbp-de-mendes.lan>
2024-07-18 17:38:52 +0200

    Add addittional checks...

--- src/Test2.java
+++ src/Test2.java
@@ -4,9 +4,9 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String hungry = "yes";
+		String hungry = "yess";
 		
-		if (hungry == "yes")
+		if (hungry == "yess")
 			System.out.println("eat something");
 		else {
 			System.out.println("Damaze");
